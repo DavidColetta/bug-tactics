b852a58c062ebecef1d9d71ecaa3af999dc24f98
ChickoonLord <67387813+DavidColetta@users.noreply.github.com>
2026-07-23 14:13:44 -0400

    Pathfinding visualization

--- navigation.gd
+++ navigation.gd
@@ -1,12 +1,61 @@
+class_name Navigation
 extends Node
 
-var gridObjects: Array[Array] #2d array
+static var instance := self
 
-func moveUnit(X, Y, toX, toY):
-	var unit := gridObjects[X][Y] as Unit
+static var tile_size := 32
+
+static var maxX := 50
+static var maxY := 50
+
+#2d array of units on the grid
+static var grid: Array[Array]
+
+#1d list of units
+static var units: Array[Unit]
+
+@export var tilemap_layer_node: TileMapLayer
+@export var visual_path_line2d: Line2D
+
+var pathfinding_grid : AStarGrid2D = AStarGrid2D.new()
+
+# call this function to move a unit around
+static func moveUnit(X, Y, toX, toY) -> void:
+	if grid[toX][toY] != null:
+		return
+	var unit := grid[X][Y] as Unit
 	if not unit:
 		return
-	gridObjects[X][Y] = null
-	gridObjects[toX][toY] = unit
+	grid[X][Y] = null
+	grid[toX][toY] = unit
 	unit.X = toX
 	unit.Y = toY
+
+func pathfind(subject: Unit, target: Vector2i):
+	visual_path_line2d.global_position = Vector2(tile_size/2.0, tile_size/2.0)
+	
+	pathfinding_grid.region = tilemap_layer_node.get_used_rect()
+	pathfinding_grid.cell_size = Vector2(tile_size, tile_size)
+	pathfinding_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	pathfinding_grid.update()
+	
+	for tile in tilemap_layer_node.get_used_cells():
+		pathfinding_grid.set_point_solid(tile)
+	
+	for unit in units:
+		pathfinding_grid.set_point_solid(Vector2i(unit.X, unit.Y), true)
+	
+	var path := pathfinding_grid.get_point_path(subject.get_pos(), target)
+	visual_path_line2d.points = path
+
+
+
+func _ready() -> void:	
+	instance = self
+	# initialize empty 2d array grid
+	grid = []
+	for i in range(maxX):
+		var column := []
+		for j in range(maxY):
+			column.append(null)
+		grid.append(column)
--- unit.gd
+++ unit.gd
@@ -1,7 +1,7 @@
 class_name Unit
 extends Node2D
 
-# position
+# position (please move unit by calling MoveUnit in Navigation)
 @export var X = 0:
 	set(new_value):
 		X = new_value
@@ -11,9 +11,16 @@ extends Node2D
 		Y = new_value
 		_on_position_updated()
 
-func _on_position_updated():
-	position.x = X * 32
-	position.y = Y * 32
+func get_pos() -> Vector2i:
+	return Vector2i(X, Y)
+
+func _on_position_updated() -> void:
+	position.x = X * Navigation.tile_size
+	position.y = Y * Navigation.tile_size
 
 func _ready() -> void:
 	_on_position_updated()
+	Navigation.grid[X][Y] = self
+	
+	#example of pathfinding
+	Navigation.instance.pathfind(self, Vector2i(2,6))
