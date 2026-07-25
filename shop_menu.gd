a92c40500abbb03d97e89c40debccd4e6e865992
Siyan Zuhayer <siyanzuhayer@gmail.com>
2026-07-25 00:50:43 -0400

    updated shop to not let new unit move and reset buttons

--- shop_menu.gd
+++ shop_menu.gd
@@ -12,4 +12,6 @@ func _on_shop_menu_item_selected(index: int) -> void:
 	var new_unit = prefabs[index].instantiate() as Unit
 	new_unit.X = Selection.selected_nest.get_pos().x
 	new_unit.Y = Selection.selected_nest.get_pos().y
+	new_unit.moved = true
 	$"../../Combatants".add_child(new_unit)
+	deselect_all()
