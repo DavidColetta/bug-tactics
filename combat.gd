b91c0bc780bb3ca26b42c15cec97a72c40d4de0d
ChickoonLord <67387813+DavidColetta@users.noreply.github.com>
2026-07-25 18:13:41 -0400

    AI

--- combat.gd
+++ combat.gd
@@ -15,8 +15,11 @@ static func get_absolute_distance_between_units(Unit1: Unit, Unit2: Unit) -> int
 	return abs(Unit1.X - Unit2.X) + abs(Unit1.Y - Unit2.Y)
 
 static func make_unit_attack_other_unit(Unit1: Unit, Unit2: Unit):
-	Unit2.HP -= ceil(Unit1.DAMAGE_TO_TYPES[Unit2.Type] * ((Unit1.HP + 10) / 110.0) )
-	Unit1.HP -= ceil(Unit2.DAMAGE_TO_TYPES[Unit1.Type] * ((Unit2.HP + 10) / 110.0) )
+	var damage_to_deal = ceil(Unit1.DAMAGE_TO_TYPES[Unit2.Type] * ((Unit1.HP + 10) / 110.0) )
+	Unit2.HP -= damage_to_deal
+	var retaliation_damage = ceil(Unit2.DAMAGE_TO_TYPES[Unit1.Type] * ((Unit2.HP + 10) / 110.0) )
+	if retaliation_damage > 0:
+		Unit1.HP -= retaliation_damage
 
 func _ready() -> void:
 	pass
