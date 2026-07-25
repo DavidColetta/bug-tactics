b4678162fc981e894eecb58dc0e2f7b5d50b86cb
ChickoonLord <67387813+DavidColetta@users.noreply.github.com>
2026-07-25 00:29:51 -0400

    Spawn ants

--- shop_menu.gd
+++ shop_menu.gd
@@ -3,5 +3,13 @@ extends ItemList
 
 static var instance := self
 
+@export var prefabs: Array[PackedScene]
+
 func _ready() -> void:
 	instance = self
+
+func _on_shop_menu_item_selected(index: int) -> void:
+	var new_unit = prefabs[index].instantiate() as Unit
+	new_unit.X = Selection.selected_nest.get_pos().x
+	new_unit.Y = Selection.selected_nest.get_pos().y
+	$"../../Combatants".add_child(new_unit)
