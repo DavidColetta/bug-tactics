5dc5c716c2b4944c3c245b7fc0a9511c227fc648
Sqwek <47760706+Sqwek@users.noreply.github.com>
2026-07-26 11:26:37 -0400

    Money On kill, and stats for units

--- unit.gd
+++ unit.gd
@@ -102,6 +102,8 @@ func _ready() -> void:
 	#move_to(Vector2i(6,4))
 
 func die() -> void:
+	if Team != Combat.Team.PLAYER:
+		Selection.player_pennies += 50
 	Navigation.grid[X][Y] = null
 	Navigation.units.erase(self)
 	queue_free()
